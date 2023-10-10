912e848c8d326013066ec0ec849e3a367a9432ab
Nxdxg <nicolasdgomar@gmail.com>
2023-10-10 14:19:31 +0200

    prueba casa 2

--- phyton/ejercicios U1/ejercicio_2-27.py
+++ phyton/ejercicios U1/ejercicio_2-27.py
@@ -2,7 +2,7 @@ producto = input("Introduce el nombre del producto, su precio y el número de un
 producto = producto.split(",")
 nombre = producto[0]
 precio = float(producto[1])
-cantidad = float(producto[2])
+cantidad = int(producto[2])
 total = precio * cantidad
 
-print(f"{nombre.title()}, su precio es de {round(precio,2)}€, con una cantidad de {round(cantidad,2)} unidades y el coste total es de: {round(total, 2)}€")
+print("El nombre del producto es: {}, su precio es de: {:6.2f}€, se han vendido un total de {:3d} unidades y el coste total es de: {:8.2f}€.".format(nombre, precio, cantidad, total))
