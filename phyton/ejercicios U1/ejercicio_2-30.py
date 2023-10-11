e910e02cf8ea9b55fe66f84c968d34f64cbd59fc
nogmalm2411 <ngomalm2411@g.educaand.es>
2023-10-11 18:50:39 +0200

    prueba con ejercicio

--- phyton/ejercicios U1/ejercicio_2-30.py
+++ phyton/ejercicios U1/ejercicio_2-30.py
@@ -1,10 +1,50 @@
+#inicio
+#   inicio = int(input("dame el numero de inicio de la serie:"))
+#   incremento = int(input("dame el numero de incremento de la serie:"))
+#   total = int(input("dame el numero total de la serie:"))
+#   if incremento < 0:
+#       while (incremento < 0):
+#           incremento = int(input("dame un numero POSITIVO de incremento de la serie:"))
+#   if total < 0:
+#       while (total < 0):
+#           total = int(input("dame un numero POSITIVO de total de la serie:"))
+#   serie = str("serie=>" + str(inicio) + "-")
+#   cont = inicio
+#   while( cont <= total):
+#       inicio = inicio + incremento
+#       cont = cont + incremento
+#       if cont < (total-1):
+#           serie =str(serie + str(inicio) + "..")
+#       else:
+#           if cont == (total - 1):
+#               serie =str(serie + str(inicio))
+#               inicio = inicio + incremento
+#               serie= str(serie) + "-" + str(inicio)
+#               print(serie)
+# final
+
 inicio = int(input("dame el numero de inicio de la serie:"))
 incremento = int(input("dame el numero de incremento de la serie:"))
 total = int(input("dame el numero total de la serie:"))
 if incremento < 0:
     while (incremento < 0):
-        incremento = int(input("dame el numero de incremento de la serie:"))
+        incremento = int(input("dame un numero POSITIVO de incremento de la serie:"))
 if total < 0:
     while (total < 0):
-        total = int(input("dame el numero de incremento de la serie:"))
-for 
+        total = int(input("dame un numero POSITIVO de total de la serie:"))
+
+#while incremento < 0 or total < 0: posible mejora  
+#    print("funciona")
+serie = str("serie=>" + str(inicio) + "-")
+cont = inicio
+while( cont <= total):
+    inicio = inicio + incremento
+    cont = cont + incremento
+    if cont < (total-1):
+        serie =str(serie + str(inicio) + "..")
+    else:
+        if cont == (total - 1):
+            serie =str(serie + str(inicio))
+            inicio = inicio + incremento
+            serie= str(serie) + "-" + str(inicio)
+            print(serie)
